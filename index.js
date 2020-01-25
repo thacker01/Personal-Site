f71c2981411337584f56d115121e6712608fa193
James Thacker <thacker01@outlook.com>
2020-01-25 14:06:39 -0500

    Minor updates to the menu list

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const menuIcon = document.getElementById('navbar-container_navbar_menu')
 const menuBackground = document.getElementById('menu-active_background')
-const menuForeground = document.getElementById('menu-active_foreground')
+const list = document.getElementById('menu-list')
+const menubars = document.querySelectorAll('.navbar-container_navbar_menu_bars')
+
+let menuactive = false
 
 menuIcon.addEventListener('click', (e)=>{
     menuBackground.classList.toggle('invisible')
-    menuForeground.classList.toggle('invisible')
+    list.classList.toggle('invisible')
+    for(x of menubars){
+        if(menuactive){
+            x.style.background = "white"
+        }else{
+            x.style.background = "grey"
+        }
+    }
+    menuactive = !menuactive
 })
 
 menuBackground.addEventListener('click', e =>{
     menuBackground.classList.toggle('invisible')
-    menuForeground.classList.toggle('invisible')
-})
-
-menuForeground.addEventListener('click', e =>{
-    menuForeground.classList.toggle('invisible')
-    menuBackground.classList.toggle('invisible')
+    list.classList.toggle('invisible')
+    for(x of menubars){
+        x.style.background = "white"
+    }
+    menuactive = !menuactive
 })
